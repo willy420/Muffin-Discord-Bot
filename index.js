316fc8878b3e1517d59afeac9cd4824d8a4a8965
Willy4 <46751321+willy420@users.noreply.github.com>
2019-07-05 15:53:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NTk2MzYxMjYyMTc5NTQ5MjUz.XR4c3A.of-Anz4ET7sKDhVdtVvlDMlDW9U';
+
+const PREFIX = '>';
+
+var version = '1.0.1';
+
+bot.on('ready', () =>{
+    console.log('Muffin is online!');
+})
+
+bot.on('message', message=>{
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+            case 'hey':
+                message.channel.sendMessage('Hello!');
+                break;
+            case 'reddit':
+                message.channel.sendMessage('https://www.reddit.com/r/CrownMuffin/');
+                break;
+            case 'info':
+                if(args[1] === 'version'){
+                    message.channel.sendMessage('Version ' + version);
+                }else{
+                    message.channel.sendMessage('Invalid arguments');
+                }
+                break;
+            case 'description':
+                message.channel.sendMessage('This is Muffin Bot , created by Willy4 - 04.07.2019');
+                break;
+    }
+})
+
+bot.login(token);
